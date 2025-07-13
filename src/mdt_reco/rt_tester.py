374ae37a2333e4aa53f6e03dcd9b7fd0512ce406
Walker Sundquist <wwsundquist@gmail.com>
2025-07-13 17:57:49 -0400

    Plot hits individually over detector geometry I'm still not sure why there is a mismatch between hit data & Geometry.py and/or the config file

--- src/mdt_reco/rt_tester.py
+++ src/mdt_reco/rt_tester.py
@@ -19,15 +19,18 @@ data_path = os.path.normpath(os.path.join(script_dir, '../../data/events_10k.npy
 data = np.load(data_path, allow_pickle=True)
 
 all_rows = []
-for event in data:
+for event_id, event in enumerate(data):
     n_hits = len(event['x'])
     for i in range(n_hits):
         row = {key: event[key][i] for key in event}
+        row["event_id"] = event_id
         all_rows.append(row)
 
 df = pd.DataFrame(all_rows)
+print(df.head(30))
+
 for i in range(len(df['drift_time'])):
-    if df['layer'][i] != 0:
+    if df['drift_time'][i] != 0:
         print("Got one!")
 
 #load configs to make geometry plot
@@ -36,14 +39,26 @@ with open("../../configs/config.yaml") as f:
 
 #show chamber
 chamber = Chamber(config)
-fig, ax = plt.subplots(figsize=(12, 8))
-chamber.Draw(ax=ax, key="channel")  # key="channel" adds text labels per tube
+#fig, ax = plt.subplots(figsize=(12, 8))
+#chamber.Draw(ax=ax, key="channel")  # key="channel" adds text labels per tube
 
 #show hits
-x_hits = df['x']
-y_hits = df['y']
-ax.scatter(x_hits, y_hits, s=200, facecolors='none', edgecolors='red', linewidths=2, label='Hits')
-ax.legend()
+x_hits_by_event = {}
 
-plt.savefig("figures/chamber_geometry.jpg", format='jpg', dpi=300)
-plt.close(fig)
+for event_id, group in df.groupby("event_id"):
+    x_hits_by_event[event_id] = group["x"].tolist()
+
+y_hits_by_event = {}
+
+for event_id, group in df.groupby("event_id"):
+    y_hits_by_event[event_id] = group["y"].tolist()
+
+for i in range(10):
+    fig, ax = plt.subplots(figsize=(12, 8))
+    chamber.Draw(ax=ax, key="channel")  # key="channel" adds text labels per tube
+    x_hits = x_hits_by_event[i]
+    y_hits = y_hits_by_event[i]
+    ax.scatter(x_hits, y_hits, s=200, facecolors='none', edgecolors='red', linewidths=2, label='Hits')
+    ax.legend()
+    plt.savefig("figures/chamber_geometry_" + str(i) + ".jpg", format='jpg', dpi=300)
+    plt.close(fig)
