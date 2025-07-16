29fa6d678b7d5a9e244b16c2d5d193207eecb9d6
DaiJon <daijon.james@stonybrook.edu>
2025-07-16 17:30:19 -0400

    rebasing from main. resolved merge conflicts, and added changes to Gen.py

--- src/mdt_reco/Gen.py
+++ src/mdt_reco/Gen.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from .Geometry import Chamber
 
 
 
@@ -15,61 +16,87 @@ class Generator:
     angle_max = np.pi/3.5
     Energy = random.gauss(4,1)
     Momentum = np.sqrt(Energy**2 - muon_mass**2) # Total muon momentum in GeV/c
+    """
+    when creating a class object you need to pass it the object config. config is defined as stated below:
 
-    def __init__(self, Chamber):
-        self.x_interval = [Chamber["locX"].min() - 30,  Chamber["locX"].max() + 30]
-        self.y_interval = [Chamber["locY"].max() + 30, Chamber["locY"].max() + 60]
-        self.chamber= Chamber
-        self.SimEvents = {
+    config_path = "/data/dhumphreys/L0MDT/mdt-reco/configs/ci_config.yaml"
+    config = mdt_reco.configParser(config_path)
+
+    """
+
+    def __init__(self, config):
+        
+        self.Chamber= Chamber(config)
+        self.x_interval = [self.Chamber["x"].min() - 30,  self.Chamber["x"].max() + 30]
+        self.y_interval = [self.Chamber["y"].max() + 30, self.Chamber["y"].max() + 60]
+        
+    def simEvent(self):
+        sim_event = {
             "pos_init": [],
             "angle_of_attack": [],
             "px": [],
             "py": []
         }
-        self.track_params={
-            "A":[],
-            "C":[],
-            "tubes_hit":[]
-
-        }
-        
-
-    def SimEvents(self):
         self.x_pos = np.random.uniform(self.x_interval[0], self.x_interval[1])
         self.y_pos = np.random.uniform(self.y_interval[0], self.y_interval[1])
     
-        self.SimEvents["pos_init"].append([self.x_pos,self.y_pos])
-        self.SimEvents["angle_of_attack"].append([np.random.uniform(-Generator.angle_max, Generator.angle_max)])
-        self.SimEvents["px"] = Generator.Momentum*np.sin(self.SimEvents["angle_of_attack"])
-        self.SimEvents["py"] = Generator.Momentum*np.cos(self.SimEvents["angle_of_attack"])
+        sim_event["pos_init"]=[self.x_pos,self.y_pos]
+        sim_event["angle_of_attack"]=np.random.uniform(-Generator.angle_max, Generator.angle_max)
+        sim_event["px"]=Generator.Momentum*np.sin(sim_event["angle_of_attack"])
+        sim_event["py"]=Generator.Momentum*np.cos(sim_event["angle_of_attack"])
         
-    def FindTrajectory(self):
+        return sim_event
+    
+    def simEvents(self,num_events):
+        sim_events=[]
+        for i in range(num_events):
+            sim_events.append(self.simEvent())
+        return sim_events
+
+
+    def findTrajectory(self, B, sim_event):
         """
         I expect this function to be run after the entire SimEvents dictionary is created. I think it will still work if this is not the case but 
         it would be easier if write some loop that develops the entire SimEvents dictionary(SimEvents() for i in range....)
         then just run FindTrajectory once as it just loops through the entirety of the dictionary in SimEvents
         """
-        for i in range(len(self.SimEvents["pos_init"])):
-            x0, y0 = self.SimEvents["pos_init"][i]
-            px = self.SimEvents["px"][i]
-            py = self.SimEvents["py"][i]
+        self.track_params={
+            "A":[],
+            "C":[],
+            "tubes_hit":[]
+        }
+        
+        if B == 0:
+        
+            x0, y0 = sim_event["pos_init"]
+            px = sim_event["px"]
+            py = sim_event["py"]
 
-        # Compute slope (A) and y-intercept (C)
+            # Compute slope (A) and y-intercept (C)
             A = py / px
             C = y0 - A * x0
 
-        # Store A and C
+            # Store A and C
             self.track_params["A"].append(A)
             self.track_params["C"].append(C)
 
-        # Calculate distance from track to all tubes
-            x = self.chamber["locX"]
-            y = self.chamber["locY"]
+            # Calculate distance from track to all tubes
+            x = self.Chamber["x"]
+            y = self.Chamber["y"]
             d = np.abs(A * x - y + C) / np.sqrt(A**2 + 1)
+            hit_candidate= np.where(d < 15)
 
-        # Determine which tubes are hit
-            tubes_hit = np.where(d < self.chamber.radius())[0]
-            self.track_params["tubes_hit"].append(tubes_hit)
+
+
+
+            # # Determine which tubes are hit
+            # tubes_hit = np.where(d < self.Chamber.radius())[0]
+            # self.track_params["tubes_hit"].append(tubes_hit)
+
+        else:
+            raise NotImplementedError
+        
+        return d
 
 
 
--- src/mdt_reco/__init__.py
+++ src/mdt_reco/__init__.py
@@ -2,7 +2,9 @@ from .ConfigParser import ConfigParser
 from .Geometry import Chamber
 from .Event import Event
 from .TrackFitter import TrackFitter
+from .Gen import Generator 
 
 configParser = ConfigParser
 geo = Chamber
 event = Event
+gen= Generator
