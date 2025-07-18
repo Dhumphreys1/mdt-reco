d54b436b7169f5151144ae4d1b2d5f1d9c153f5a
Bernadette Starzee <bstarzee@vl965-172-31-2-251.wireless.umass.edu>
2025-07-18 16:36:59 -0400

    Added an executable for the encoder and decoder

--- src/mdt_reco/__init__.py
+++ src/mdt_reco/__init__.py
@@ -3,9 +3,11 @@ from .Event import Event
 from .Gen import Generator
 from .Geometry import Chamber
 from .TrackFitter import TrackFitter
+from .Signal import Signal
 
 configParser = ConfigParser
 geo = Chamber
 event = Event
 gen = Generator
 trackFitter = TrackFitter
+Signal = Signal
